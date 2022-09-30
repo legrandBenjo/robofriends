c8044576bfcf7c9dea5579f516f4500596272e51
legrandBenjo <coupec.legrand@outlook.com>
2022-09-30 19:49:36 +0200

    Updates

---
+++ precache-manifest.8d57b06a36aab285d10a5cc9cb90e00c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "daa1c66084355fec693f",
+    "url": "/robofriends/static/css/main.0a1113e7.chunk.css"
+  },
+  {
+    "revision": "daa1c66084355fec693f",
+    "url": "/robofriends/static/js/main.daa1c660.chunk.js"
+  },
+  {
+    "revision": "0566a01f8352d9043b6f",
+    "url": "/robofriends/static/css/1.4b45c062.chunk.css"
+  },
+  {
+    "revision": "0566a01f8352d9043b6f",
+    "url": "/robofriends/static/js/1.0566a01f.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "3f0098af73cd52ac8a883b6b515e0f26",
+    "url": "/robofriends/static/media/BloodyTerror-GOW9Z.3f0098af.woff"
+  },
+  {
+    "revision": "6d4560f79a6532d7cf639579a2e0ee3d",
+    "url": "/robofriends/index.html"
+  }
+];
